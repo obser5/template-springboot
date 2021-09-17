65767f004b2a895efe2073ba2c5eac6a2d9a8d3f
obser5 <961767524@qq.com>
2021-09-17 16:04:38 +0800

    修改了测试类，避免打包失败

--- src/test/java/com/han/TemplateSpringbootApplicationTests.java
+++ src/test/java/com/han/TemplateSpringbootApplicationTests.java
@@ -1,28 +1,13 @@
 package com.han;
 
-import com.han.mapper.MyUserMapper;
-import com.han.model.MyUser;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TemplateSpringbootApplicationTests {
 
-    private static final Logger logger = LoggerFactory.getLogger(TemplateSpringbootApplicationTests.class);
-    /**
-     * 字段注入
-     */
-    @Autowired
-    private MyUserMapper myUserMapper;
-
     @Test
     void myUserTest() {
-        for (MyUser myUser : myUserMapper.listAllMyUsers()) {
-            logger.info("测试成功！");
-            System.out.println(myUser);
-        }
+        System.out.println("Hello World!");
     }
 }
